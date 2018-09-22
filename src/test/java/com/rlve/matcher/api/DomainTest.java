675c29236ea70c37f62ca8f8a60592edffd7cfc9
rlve <radioslav@gmail.com>
2018-09-22 18:37:14 +0200

    more Domain Tests

--- src/test/java/com/rlve/matcher/api/DomainTest.java
+++ src/test/java/com/rlve/matcher/api/DomainTest.java
@@ -2,24 +2,26 @@ package com.rlve.matcher.api;
 
 import com.rlve.matcher.api.domain.Details;
 import com.rlve.matcher.api.domain.Match;
-import com.rlve.matcher.api.domain.MatchState;
 import com.rlve.matcher.api.domain.User;
 import com.rlve.matcher.api.repositories.DetailsRepository;
 import com.rlve.matcher.api.repositories.MatchRepository;
 import com.rlve.matcher.api.repositories.UserRepository;
-import com.rlve.matcher.api.services.SignService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.unitils.reflectionassert.ReflectionComparatorMode;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.unitils.reflectionassert.ReflectionAssert.*;
 
 @RunWith(SpringRunner.class)
 @DataNeo4jTest
@@ -33,10 +35,10 @@ public class DomainTest {
 
     private static final String NAME = "John Test";
     private static final String PLACE = "Test Place";
-    private static final Instant matchDate =
+    private static final Instant MATCH_DATE =
             LocalDateTime.of(2018, 11, 15, 18, 0)
             .atZone(ZoneId.systemDefault())
-            .toInstant();;
+            .toInstant();
 
     @Test
     public void shouldAllowUserCreation() {
@@ -50,7 +52,7 @@ public class DomainTest {
 
     @Test
     public void shouldAllowMatchCreation() {
-        Match match = new Match(PLACE, matchDate, 20);
+        Match match = new Match(PLACE, MATCH_DATE, 20);
         match = matchRepository.save(match);
 
         Match matchFromDb = matchRepository.findById(match.getId()).orElseThrow();
@@ -63,7 +65,7 @@ public class DomainTest {
     @Test
     public void shouldAllowDetailsCreation() {
         User user = new User(NAME);
-        Match match = new Match(PLACE, matchDate, 20);
+        Match match = new Match(PLACE, MATCH_DATE, 20);
 
         Details details = new Details(match, user);
         details = detailsRepository.save(details);
@@ -75,7 +77,7 @@ public class DomainTest {
     @Test
     public void shouldAllowDetailsToCreateMatchAndUser() {
         User user = new User(NAME);
-        Match match = new Match(PLACE, matchDate, 20);
+        Match match = new Match(PLACE, MATCH_DATE, 20);
 
         Details details = new Details(match, user);
         details = detailsRepository.save(details);
@@ -90,5 +92,45 @@ public class DomainTest {
         assertEquals(match.getMatchDate(), matchesFromDb.iterator().next().getMatchDate());
     }
 
+    @Test
+    public void shouldKeepUserDataUnchanged() {
+        User user = new User(NAME);
+        user = userRepository.save(user);
+
+        User userFromDb = userRepository.findById(user.getId()).orElseThrow();
+        assertReflectionEquals(user, userFromDb, ReflectionComparatorMode.LENIENT_ORDER);
+    }
+
+    @Test
+    public void shouldKeepMatchDataUnchanged() {
+        Match match = new Match(PLACE, MATCH_DATE, 20);
+        match.setCost(200);
+        match.setMinPlayers(10);
+        match.setSquad(Arrays.asList(1L, 2L, 3L, 4L));
+        match.setReserves(Arrays.asList(5L, 6L, 7L, 8L));
+        match = matchRepository.save(match);
+
+        Match matchFromDb = matchRepository.findById(match.getId()).orElseThrow();
+        assertReflectionEquals(match, matchFromDb, ReflectionComparatorMode.LENIENT_ORDER);
+    }
+
+    @Test
+    public void shouldKeepDetailsDataUnchanged() {
+        User user = new User(NAME);
+        user = userRepository.save(user);
+
+        Match match = new Match(PLACE, MATCH_DATE, 20);
+        match = matchRepository.save(match);
+
+        Details details = new Details(match, user);
+        details.setInSquad(Boolean.TRUE);
+        details.setInReserves(Boolean.FALSE);
+        details.setUserPaid(Boolean.FALSE);
+        details.setUserPresent(Boolean.TRUE);
+        details = detailsRepository.save(details);
+
+        Details detailsFromDb = detailsRepository.findById(details.getId()).orElseThrow();
+        assertReflectionEquals(details, detailsFromDb, ReflectionComparatorMode.LENIENT_ORDER);
+    }
 
 }
