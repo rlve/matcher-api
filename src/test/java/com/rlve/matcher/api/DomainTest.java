798e5ec29eac1a7782d210bd2967858408ccc1e2
rlve <radioslav@gmail.com>
2018-09-23 15:20:45 +0200

    refactor Domain tests

--- src/test/java/com/rlve/matcher/api/DomainTest.java
+++ src/test/java/com/rlve/matcher/api/DomainTest.java
@@ -16,7 +16,6 @@ import org.unitils.reflectionassert.ReflectionComparatorMode;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Set;
 
@@ -39,97 +38,116 @@ public class DomainTest {
             LocalDateTime.of(2018, 11, 15, 18, 0)
             .atZone(ZoneId.systemDefault())
             .toInstant();
+    private static final Integer MAX_PLAYERS = 20;
 
     @Test
     public void shouldAllowUserCreation() {
+        // given
         User user = new User(NAME);
-        user = userRepository.save(user);
 
+        // when
+        user = userRepository.save(user);
         User userFromDb = userRepository.findById(user.getId()).orElseThrow();
-        assertEquals(user.getId(), userFromDb.getId());
-        assertEquals(user.getName(), userFromDb.getName());
+
+        // then
+        assertReflectionEquals(user, userFromDb);
     }
 
     @Test
     public void shouldAllowMatchCreation() {
-        Match match = new Match(PLACE, MATCH_DATE, 20);
-        match = matchRepository.save(match);
+        // given
+        Match match = new Match(PLACE, MATCH_DATE, MAX_PLAYERS);
 
+        // when
+        match = matchRepository.save(match);
         Match matchFromDb = matchRepository.findById(match.getId()).orElseThrow();
-        assertEquals(match.getId(), matchFromDb.getId());
-        assertEquals(match.getPlace(), matchFromDb.getPlace());
-        assertEquals(match.getMatchDate(), matchFromDb.getMatchDate());
-        assertEquals(match.getMaxPlayers(), matchFromDb.getMaxPlayers());
+
+        // then
+        assertReflectionEquals(match, matchFromDb);
     }
 
     @Test
     public void shouldAllowDetailsCreation() {
+        // given
         User user = new User(NAME);
-        Match match = new Match(PLACE, MATCH_DATE, 20);
-
+        Match match = new Match(PLACE, MATCH_DATE, MAX_PLAYERS);
         Details details = new Details(match, user);
-        details = detailsRepository.save(details);
 
+        // when
+        details = detailsRepository.save(details);
         Details detailsFromDb = detailsRepository.findByUserIdAndMatchId(user.getId(), match.getId()).orElseThrow();
-        assertEquals(details.getId(), detailsFromDb.getId());
+
+        // then
+        assertReflectionEquals(details, detailsFromDb);
     }
 
     @Test
     public void shouldAllowDetailsToCreateMatchAndUser() {
+        // given
         User user = new User(NAME);
-        Match match = new Match(PLACE, MATCH_DATE, 20);
-
+        Match match = new Match(PLACE, MATCH_DATE, MAX_PLAYERS);
         Details details = new Details(match, user);
-        details = detailsRepository.save(details);
 
+        // when
+        detailsRepository.save(details);
         Set<User> usersFromDb = userRepository.findAllByNameLike(NAME);
-        assertEquals(usersFromDb.size(), 1);
+        Set<Match> matchesFromDb = matchRepository.findAllByPlace(PLACE);
+
+        // then
+        assertEquals(1, usersFromDb.size());
         assertEquals(user.getName(), usersFromDb.iterator().next().getName());
 
-        Set<Match> matchesFromDb = matchRepository.findAllByPlace(PLACE);
-        assertEquals(matchesFromDb.size(), 1);
+        assertEquals(1, matchesFromDb.size());
         assertEquals(match.getPlace(), matchesFromDb.iterator().next().getPlace());
         assertEquals(match.getMatchDate(), matchesFromDb.iterator().next().getMatchDate());
     }
 
     @Test
     public void shouldKeepUserDataUnchanged() {
+        // given
         User user = new User(NAME);
         user = userRepository.save(user);
 
+        // when
         User userFromDb = userRepository.findById(user.getId()).orElseThrow();
+
+        // then
         assertReflectionEquals(user, userFromDb, ReflectionComparatorMode.LENIENT_ORDER);
     }
 
     @Test
     public void shouldKeepMatchDataUnchanged() {
-        Match match = new Match(PLACE, MATCH_DATE, 20);
+        // given
+        Match match = new Match(PLACE, MATCH_DATE, MAX_PLAYERS);
         match.setCost(200);
         match.setMinPlayers(10);
         match.setSquad(Arrays.asList(1L, 2L, 3L, 4L));
         match.setReserves(Arrays.asList(5L, 6L, 7L, 8L));
         match = matchRepository.save(match);
 
+        // when
         Match matchFromDb = matchRepository.findById(match.getId()).orElseThrow();
+
+        // then
         assertReflectionEquals(match, matchFromDb, ReflectionComparatorMode.LENIENT_ORDER);
     }
 
     @Test
     public void shouldKeepDetailsDataUnchanged() {
+        // given
         User user = new User(NAME);
-        user = userRepository.save(user);
-
-        Match match = new Match(PLACE, MATCH_DATE, 20);
-        match = matchRepository.save(match);
-
+        Match match = new Match(PLACE, MATCH_DATE, MAX_PLAYERS);
         Details details = new Details(match, user);
         details.setInSquad(Boolean.TRUE);
         details.setInReserves(Boolean.FALSE);
         details.setUserPaid(Boolean.FALSE);
         details.setUserPresent(Boolean.TRUE);
-        details = detailsRepository.save(details);
 
+        // when
+        details = detailsRepository.save(details);
         Details detailsFromDb = detailsRepository.findById(details.getId()).orElseThrow();
+
+        // then
         assertReflectionEquals(details, detailsFromDb, ReflectionComparatorMode.LENIENT_ORDER);
     }
 
